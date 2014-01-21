f32636f010df6eaba4b8bc9ccc85996cfcb73d20
No-Rona <noronharohan94@gmail.com>
2014-01-21 21:51:26 +0530

    Addeed full damn shit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-/*The javascript/jquery to be used on the site*/
+$(document).ready(function(){
+var showing=true;
+var count=0;
+$( ".armymove" ).draggable();
+
+	
+	$('#showcity').click(function(){
+	
+		if(showing){
+			$('.cities').css('display','block');
+			showing=false;
+		}
+		
+		else{
+			$('.cities').css('display','none');
+			$('.routes').css('display','none');
+			showing=true;
+		}
+	});
+	
+	$('#showroute').click(function(){
+	
+		if(showing){
+			$('.routes').css('display','block');
+			showing=false;
+		}
+		
+		else{
+			$('.routes').css('display','none');
+			$('.cities').css('display','none');
+			showing=true;
+		}
+	});
+	
+	
+	$('createarmy').click(function(){
+	
+		$('#placearmy').html('<p>'+ count + '</p>');
+		count++;
+	});
+	
+	$( ".armymove" ).draggable();
+	
+});
+
+var troll= new function(){
+document.getElementByID('placearmy').innerHTML("<p>this is cunt</p>");
+};
